a984389f4853e5f53b60d5b9562b8b0cf8e2270f
armanschar <ascharbay@hotmail.com>
2025-02-01 21:18:44 -0400

    Add files via upload

---
+++ DryApp/DryApp/Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace DryApp
+{
+    public class Payroll
+    {
+        public static void Main(string[] args)
+        {
+            Run();
+        }
+
+        public static void Run()
+        {
+            Console.WriteLine("Seleccione el tipo de empleado (1: Tiempo completo, 2: Medio tiempo):");
+            string employeeType = Console.ReadLine();
+
+            switch (employeeType)
+            {
+                case "1":
+                    
+                    ProcesoTiempoCompleto();
+                    break;
+                case "2":
+                    
+                    ProcesoMedioTiempo();
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida. Seleccione 1 o 2.");
+                    break;
+            }
+        }
+        private static void ProcesoTiempoCompleto()
+        {
+            Console.WriteLine("Ingrese el salario base:");
+            string salarioBaseInput = Console.ReadLine();
+            if (decimal.TryParse(salarioBaseInput, out decimal salarioBase))
+            {
+                decimal salarioNeto = CalcSalarioNeto(salarioBase);
+                Console.WriteLine($"Salario neto después de impuestos y bono: {salarioNeto}");
+            }
+            else
+            {
+                Console.WriteLine("Por favor, ingrese un valor válido para el salario base.");
+            }
+        }
+
+        private static void ProcesoMedioTiempo()
+        {
+            Console.WriteLine("Ingrese el sueldo por hora:");
+            string porHoraInput = Console.ReadLine();
+            if (decimal.TryParse(porHoraInput, out decimal porHora))
+            {
+                Console.WriteLine("Ingrese las horas trabajadas:");
+                string horasTrabajadasInput = Console.ReadLine();
+                if (int.TryParse(horasTrabajadasInput, out int horasTrabajadas))
+                {
+                    decimal salarioBruto = porHora * horasTrabajadas;
+                    decimal salarioNeto = CalcSalarioNeto(salarioBruto);
+                    Console.WriteLine($"Salario neto después de impuestos y bono: {salarioNeto}");
+                }
+                else
+                {
+                    Console.WriteLine("Por favor, ingrese un valor válido para las horas trabajadas.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Por favor, ingrese un valor válido para el sueldo por hora.");
+            }
+        }
+        private static decimal CalcSalarioNeto(decimal salarioBruto)
+        {
+            decimal tax = salarioBruto * 0.18m;  
+            decimal bonus = salarioBruto * 0.05m; 
+            return salarioBruto - tax + bonus;   
+        }
+
+    }
+
+}
+
---
+++ YagniApp/YagniApp/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace YagniApp
+{
+    public class ProductService
+    {
+        public void AddProduct(string name, decimal price)
+        {
+            Console.WriteLine($"Producto de nombre {name} y precio {price} añadido.");
+
+        }
+
+        public void DeleteProduct(int productId)
+        {
+            Console.WriteLine($"Producto {productId} borrado.");
+
+        }
+
+    }
+      static
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ProductService servicio = new ProductService();
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("\nSeleccione una opción:");
+                Console.WriteLine("1. Agregar producto");
+                Console.WriteLine("2. Eliminar producto");
+                Console.WriteLine("3. Salir");
+                Console.Write("Opción: ");
+                string opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        Console.WriteLine("Ingrese el nombre del producto: ");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Ingrese el precio del producto: ");
+                        if (decimal.TryParse(Console.ReadLine(), out decimal price))
+                        {
+                            servicio.AddProduct(name, price);
+                        }
+                        else
+                        {
+                            Console.WriteLine("El precio ingresado no es valido.");
+
+                        }
+                        break;
+                    case "2":
+                        Console.Write("Ingrese el ID del producto a eliminar: ");
+                        if (int.TryParse(Console.ReadLine(), out int productId))
+                        {
+                            servicio.DeleteProduct(productId);
+                        }
+                        else
+                        {
+                            Console.WriteLine("El ID ingresado no es válido.");
+                        }
+                        break;
+
+                    case "3":
+                        exit = true;
+                        Console.WriteLine("Saliendo del sistema...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción no válida. Intente nuevamente.");
+                        break;
+
+                }
+            }
+        }
+    }
+}
